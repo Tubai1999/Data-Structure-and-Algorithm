2873a03863d1470b1fd4a24dc029984f6e773113
Tridib Samanta <tridibsamanta311@gmail.com>
2025-01-03 19:14:30 +0530

    Day 4 rotation array

---
+++ src/gfg160/Day4.java
@@ -0,0 +1,35 @@
+package gfg160;
+
+public class Day4 {
+    static void reverseSubArray(int[] arr, int start, int end){
+        while(start<end){
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;end--;
+        }
+    }
+
+
+
+    static void rotateArr(int arr[], int d) {
+
+        d = d%arr.length;
+        reverseSubArray(arr,0,d-1);
+        reverseSubArray(arr,d,arr.length-1);
+        reverseSubArray(arr,0,arr.length-1);
+
+
+
+        //  with extra space.
+        // int n = arr.length;
+        // int[] doubleArray = new int[n*2];
+        // for(int i=0;i<2*n;i++){
+        //     doubleArray[i] = arr[i%n];
+        // }
+
+        // for(int i=0;i<n;i++){
+        //     arr[i] = doubleArray[(i+d)%(2*n)];
+        // }
+    }
+}
