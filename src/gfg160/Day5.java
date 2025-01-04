8e72e9ae5bf7a08e8c56e99c1a47523f2b831e1c
Tridib Samanta <tridibsamanta311@gmail.com>
2025-01-04 23:40:32 +0530

    Day5 next permutation

---
+++ src/gfg160/Day5.java
@@ -0,0 +1,35 @@
+package gfg160;
+
+public class Day5 {
+    void nextPermutation(int[] arr) {
+        // code here
+        int n = arr.length;
+        int i = n-2;
+        while(i>=0 && arr[i]>=arr[i+1]){
+            i--;
+        }
+        if(i>=0){
+            int j = n-1;
+            while(arr[i] >= arr[j]){
+                j--;
+            }
+            swapElement(arr,i,j);
+        }
+        reverseChunk(arr,i+1,n-1);
+
+    }
+
+    void reverseChunk(int[] arr, int start, int end){
+
+        while(start < end){
+            swapElement(arr,start,end);
+            start++;end--;
+        }
+    }
+
+    void swapElement(int[] arr, int start, int end){
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+    }
+}
