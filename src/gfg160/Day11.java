dab84a0b86e95fe1b342d9820c9220195617b9e6
Tridib Samanta <tridibsamanta311@gmail.com>
2025-01-10 23:32:49 +0530

    Day11 maximum product sumarray

---
+++ src/gfg160/Day11.java
@@ -0,0 +1,22 @@
+package gfg160;
+
+public class Day11 {
+    int maxProduct(int[] arr) {
+        // code here
+        int ans = Integer.MIN_VALUE;
+        int suffix = 1;
+        int prefix = 1;
+        int n = arr.length;
+
+        for(int i=0;i<arr.length;i++){
+            if(suffix == 0) suffix = 1;
+            if(prefix == 0) prefix = 1;
+            prefix = prefix * arr[i];
+            suffix = suffix * arr[n-1-i];
+
+            ans = Math.max(ans,Math.max(suffix,prefix));
+        }
+
+        return ans;
+    }
+}
