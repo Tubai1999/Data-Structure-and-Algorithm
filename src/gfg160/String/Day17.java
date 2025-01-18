6a2587a39836d981cde2080f66c8e41369fbb5e7
Tridib Samanta <tridibsamanta311@gmail.com>
2025-01-18 23:21:03 +0530

    non repetaing character

--- src/gfg160/String/Day17.java
+++ src/gfg160/String/Day17.java
@@ -10,7 +10,6 @@ public class Day17 {
             char c = s.charAt(i);
             frequencyArr[c-'a'] ++;
         }
-
         for(int i=0;i<s.length();i++){
             char c = s.charAt(i);
             if(frequencyArr[c-'a']  == 1) return c;
