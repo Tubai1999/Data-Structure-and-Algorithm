35622cd1cfe6b3cb06b5cd8334242bf7440c9f8f
Tridib Samanta <tridibsamanta311@gmail.com>
2025-02-09 23:20:53 +0530

    inorder - morris traversal

---
+++ src/GFG_POTD/MorrisTraversal.java
@@ -0,0 +1,40 @@
+package GFG_POTD;
+import java.util.*;
+
+public class MorrisTraversal {
+    ArrayList<Integer> inOrder(Node root) {
+        // Code
+        ArrayList<Integer> ans = new ArrayList<>();
+        inorderHelper(root, ans);
+        return ans;
+
+    }
+
+    //Morris Traversal
+    void inorderHelper(Node root, ArrayList<Integer> ans){
+        Node cur = root;
+        while(cur != null){
+            if(cur.left == null){
+                ans.add(cur.data);
+                cur = cur.right;
+            }
+            else{
+                Node prev = cur.left;
+                while(prev.right != null && prev.right != cur){
+                    prev = prev.right;
+                }
+                if(prev.right == null){
+                    prev.right = cur;
+                    cur = cur.left;
+                }
+                else{
+                    prev.right = null;
+                    ans.add(cur.data);
+                    cur = cur.right;
+                }
+
+
+            }
+        }
+    }
+}
