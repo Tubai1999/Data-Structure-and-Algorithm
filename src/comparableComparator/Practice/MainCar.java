3205ba8aa106d000cc9d4a2f0eb362c0c5ec0a61
Tridib Samanta <tridibsamanta311@gmail.com>
2025-03-31 22:43:04 +0530

    comaparable practice

---
+++ src/comparableComparator/Practice/MainCar.java
@@ -0,0 +1,20 @@
+package comparableComparator.Practice;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class MainCar {
+    public static void main(String[] args) {
+        List<Car> carList = new ArrayList<>();
+        carList.add(new Car("sedan","petrol"));
+        carList.add(new Car("suv","diesel"));
+        carList.add(new Car("xuv","cng"));
+
+//        Arrays.sort(carList,(Car a,Car b) -> a.engine.compareTo(b.engine));
+    Collections.sort(carList,(Car a, Car b) -> a.engine.compareTo(b.engine));
+
+        System.out.println(carList);
+    }
+}
