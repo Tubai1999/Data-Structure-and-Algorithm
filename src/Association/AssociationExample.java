530d2c3de45d063de419498762ece0fa959a725e
Tridib Samanta <tridibsamanta311@gmail.com>
2025-05-16 23:49:02 +0530

    combination 2

---
+++ src/Association/AssociationExample.java
@@ -0,0 +1,28 @@
+package Association;
+
+class Car{
+    String model;
+    Car(String model){
+        model = this.model;
+    }
+    void display(){
+        System.out.println("inside car class" + model);
+    }
+}
+
+class Person{
+    String name;
+    Car car; //has a relation, Person has Car object
+    Person(String name, Car car){
+        this.name = name;
+        this.car = car;
+    }
+}
+
+
+public class AssociationExample {
+    public static void main(String[] args) {
+        Car car = new Car("Swift");
+        Person person = new Person("Tridib", car);
+    }
+}
