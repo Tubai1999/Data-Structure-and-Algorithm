9a670920004cfdd1fe69bce8bb50c864a35b7d5f
Tridib Samanta <tridibsamanta311@gmail.com>
2025-06-14 23:41:59 +0530

    commented out

--- src/Functional_interface/function/MainFunction.java
+++ src/Functional_interface/function/MainFunction.java
@@ -1,5 +1,6 @@
 package Functional_interface.function;
 
+//commented
 public class MainFunction {
     public static void main(String[] args) {
         Function<String,Integer> obj = (Integer a)->{
