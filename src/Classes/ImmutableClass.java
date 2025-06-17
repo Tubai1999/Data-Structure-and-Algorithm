6a673d2b22013220820fa93b7f6c201953dcd527
Tridib Samanta <tridibsamanta311@gmail.com>
2025-06-17 21:34:58 +0530

    rod cutting

--- src/Classes/ImmutableClass.java
+++ src/Classes/ImmutableClass.java
@@ -1,21 +1,32 @@
 package Classes;
 
-
   class Upper{
     private final int id;
     private final String name;
-    Upper(int id, String name){
+      @Override
+      public String toString() {
+          return "Upper{" +
+                  "id=" + id +
+                  ", name='" + name + '\'' +
+                  '}';
+      }
+      Upper(int id, String name){
         this.id = id;
         this.name = name;
     }
-
 }
 
-public class ImmutableClass {
+//
 
+public class ImmutableClass extends Upper {
+    ImmutableClass(int id, String name) {
+        super(id, name);
+    }
 
     public static void main(String[] args) {
         Upper obj = new Upper(1,"tridib");
-//        ImmutableClass obj = new ImmutableClass();
+
+        ImmutableClass newObj = new ImmutableClass(4,"samanta");
+        System.out.println(obj);
     }
 }
