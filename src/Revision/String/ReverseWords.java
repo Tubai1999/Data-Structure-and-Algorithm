18f541537fdbe726b16dbd1097bd8f08f499d7b6
Tridib Samanta <tridibsamanta311@gmail.com>
2025-12-29 23:07:16 +0530

    29th day1

--- src/Revision/String/ReverseWords.java
+++ src/Revision/String/ReverseWords.java
@@ -2,6 +2,9 @@ package Revision.String;
 
 public class ReverseWords {
     public static void main(String[] args) {
-
+            char cj = 99;
+            int val = 'A';
+        System.out.println(cj);
+        System.out.println(val);
     }
 }
