ac0d7e582ccb2554d8917380586169ebe4e56d64
Tridib Samanta <tridibsamanta311@gmail.com>
2026-01-06 00:56:27 +0530

    LCS

--- src/Revise/String/LongestCommonPrefix.java
+++ src/Revise/String/LongestCommonPrefix.java
@@ -6,15 +6,18 @@ public class LongestCommonPrefix {
     public static void main(String[] args) {
 //        ArrayList<String> strArra = {"flower", "flow", "flight"};
         String[] arr = {"flower", "flow", "flight"};
-        String s = arr[0];
-        int index=0;
-        for(int i=1;i<arr.length;i++){
-            index=0;
-            while (index<arr[i].length() && index<s.length()){
-                if(arr[i].charAt(index) != s.charAt(index)) break;
-                index++;
+        String ansStr = arr[0];
+        for(int i=1;i< arr.length;i++){
+            String comStr=arr[i];
+            int j=0; //int k=0;
+            while(j<ansStr.length() && j<comStr.length()){
+                if(ansStr.charAt(j) == comStr.charAt(j)){
+                    j++;
+                }
+                else break;
             }
+            ansStr = ansStr.substring(0,j);
+            System.out.println(ansStr);
         }
-        System.out.println(index);
     }
 }
